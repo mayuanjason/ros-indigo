cc3fbad609b902918178ab490928d10253f425f7
yuanm <mayuan_1999@163.com>
2017-09-10 09:17:44 +0800

    add learning_tf2 package

---
+++ learning_tf2/nodes/static_turtle_tf2_broadcaster.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import rospy
+
+# to get commandline arguments
+import sys
+
+# Because of transformations
+import tf
+
+import tf2_ros
+import geometry_msgs.msg
+
+if __name__ == '__main__':
+    
